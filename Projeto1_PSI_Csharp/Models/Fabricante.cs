04d182c713856b3698e776faf7cdbd3fe316e4e7
Aris402 <aris4455@hotmail.com>
2021-07-01 22:58:11 -0300

    Adição de Fabricante

---
+++ Projeto1_PSI_Csharp/Models/Fabricante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto1_PSI_Csharp.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+        public string Nome { get; set; }
+    }
+}
