85b181d881f887560b7dbcd8cff950a2b2e496ac
Aris402 <aris4455@hotmail.com>
2021-07-25 14:43:38 -0300

    Capítulo 4

--- Projeto1_PSI_Csharp/Models/Fabricante.cs
+++ Projeto1_PSI_Csharp/Models/Fabricante.cs
@@ -9,5 +9,6 @@ namespace Projeto1_PSI_Csharp.Models
     {
         public long FabricanteId { get; set; }
         public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
     }
 }
